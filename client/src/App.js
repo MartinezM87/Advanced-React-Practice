7aa43517733e621008d88996721e043ed5d718c5
Martinezm87 <Martinezm0087@gmail.com>
2020-02-13 19:18:23 -0500

    final

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,40 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import axios from "axios";
 import './App.css';
 
-function App() {
-  
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      players: []
+    }
+  }
+
+
+  object = {
+    practice: {
+      number:1
+    }
+  };
+
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/api/footballers')
+    .then(res => {
+      this.setState({players: res.data});
+      console.table(res.data);
+      console.log(Array.isArray(this.state.players));
+    })
+    .catch()
+  }
+
+
+    render(){
     return (
     <div className="App">
       <h1>Happy building <span role="img" aria-label="Smiling emoji">😃</span></h1>
     </div>
   );
-}
+}}
 
 export default App;
